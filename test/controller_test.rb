b1afcc0de5ed7a59b0d58dfd5de6e2a306514d5b
Ryan Kinderman <ryan@kinderman.net>
2007-02-08 23:40:47 +0000

    added named route test

--- test/controller_test.rb
+++ test/controller_test.rb
@@ -15,6 +15,7 @@ require 'fileutils'
 class TestController < ActionController::Base  
   def rescue_action(e) raise e end
   
+  def primer; render :nothing => true; end
   def some_action
     render :text => url_for(params)
   end
@@ -27,6 +28,13 @@ class ControllerTest < Test::Unit::TestCase
     @controller = TestController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    
+    ActionController::Routing::Routes.draw do |map|
+      map.some_action_test '/test/some_action', :controller => 'test', :action => 'some_action'
+      map.connect ':controller/:action/:id'
+    end
+    
+    get :primer
   end
 
   # def test_incorrect_encoding_for_nested_hash_params
@@ -39,6 +47,14 @@ class ControllerTest < Test::Unit::TestCase
     assert_equal "http://test.host/test/some_action?some%5Bnested%5D=hash", @response.body
   end
 
+  # def test_incorrect_encoding_for_named_route
+  #   assert_equal 'http://test.host/test/some_action?some=nestedhash', some_action_test_url(:some => { :nested => :hash })
+  # end
+
+  def test_correct_encoding_for_nested_hash_params_with_named_route
+    assert_equal 'http://test.host/test/some_action?some%5Bnested%5D=hash', some_action_test_url(:some => { :nested => :hash })
+  end
+
   # def test_incorrect_encoding_for_array_params
   #   get :some_action, :an_array => [1, 2]
   #   assert_equal "http://test.host/test/some_action?an_array=1%2F2", @response.body
@@ -49,6 +65,14 @@ class ControllerTest < Test::Unit::TestCase
     get :some_action, :an_array => [1, 2]
     assert_equal "http://test.host/test/some_action?an_array[]=1&an_array[]=2", @response.body
     assert_equal ActionController::Routing::PathSegment::Result, @request.query_parameters[:an_array].class
+  end
+
+  # def test_incorrect_encoding_for_array_params_with_named_route
+  #   assert_equal 'http://test.host/test/some_action?an_array=1%2F2', some_action_test_url(:an_array => [1, 2])
+  # end
+  
+  def test_correct_encoding_for_array_params_with_named_route
+    assert_equal 'http://test.host/test/some_action?an_array[]=1&an_array[]=2', some_action_test_url(:an_array => [1, 2])
   end
   
   
